4ef8437a0a388490212c311debf2dfafcd908231
Cook1619 <cook6348@gmail.com>
2020-02-16 19:05:10 -0600

    Setting up parent component and child

--- src/app.js
+++ src/app.js
@@ -1,54 +1,67 @@
-
-class Header extends React.Component{
-    render(){
-        return (
-            <div>
-                <h1>Indecision</h1>
-                <h2>Put your life in the hands of a computer</h2>
-            </div>
-        )
-    }
-}
-
-
-class Action extends React.Component{
-    render(){
-        return (
-            <div>
-                <button>What should I do?</button>
-            </div>
-        )
-    }
-}
-
-class Options extends React.Component{
-    render(){
-        return (
-            <div>
-             Options component here
-            </div>
-        )
-    }
-}
-
-class AddOption extends React.Component{
-    render(){
-        return (
-            <div>
-                <input type="text" placeholder="Add options here"/>
-            </div>
-        )
-    }
-}
-
-
-const jsx = (
-    <div>
-        <Header />
+class IndecisionApp extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header title={}/>
         <Action />
         <Options />
         <AddOption />
-    </div>
-)
+      </div>
+    );
+  }
+}
 
-ReactDOM.render(jsx, document.getElementById('app'))
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Indecision</h1>
+        <h2>Put your life in the hands of a computer</h2>
+      </div>
+    );
+  }
+}
+
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button>What should I do?</button>
+      </div>
+    );
+  }
+}
+
+class Options extends React.Component {
+  render() {
+    return (
+        <div>
+            Option component here
+            <Option />
+        </div>
+    )
+  }
+}
+
+class Option extends React.Component {
+    render(){
+        return (
+            <div>
+                Options here
+            </div>
+        )
+    }
+}
+
+class AddOption extends React.Component {
+  render() {
+    return (
+      <div>
+        <input type="text" placeholder="Add options here" />
+      </div>
+    );
+  }
+}
+
+
+ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
